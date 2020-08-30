161d6520080d0205d4df9feab4d062ac4998e838
Alejo <alejogodoy0998@gmail.com>
2020-08-30 10:47:52 -0300

    workspace

--- sindicato/src/sindicato.c
+++ sindicato/src/sindicato.c
@@ -2,7 +2,7 @@
 int main(void){
 //ESTE VA A ACTUAR DE SERVER
      t_log* logger = log_create("./cfg/sindicato.log", "SINDICATO", true, LOG_LEVEL_INFO);
-    log_info(logger, "Soy el SINDICATO Y AGUANTE MOYANO! %s", mi_funcion_compartida());
+    log_info(logger, "Soy el SINDICATO %s", mi_funcion_compartida());
 
      inicio_server("127.0.0.1", "6010",logger);
 

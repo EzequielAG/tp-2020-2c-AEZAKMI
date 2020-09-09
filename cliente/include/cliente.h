76438f8ce1e9f9ceffba9416d28b3f993e7e106d
EzequielAG <gaunaezequiel.a@gmail.com>
2020-09-09 01:14:57 -0300

    [ADD] Agrega a cliente.h la estructura de la configuracion y las funciones necesarias

--- cliente/include/cliente.h
+++ cliente/include/cliente.h
@@ -6,4 +6,30 @@
 #include "shared_utils.h"
 #include "server.h"
 #include "tests.h"
+
+// VARIABLES Y ESTRUCTURAS
+typedef struct {
+    char* ip_comanda;
+    char* puerto_comanda;
+    char* ip_restaurante;
+    char* puerto_restaurante;
+    char* ip_sindicato;
+    char* puerto_sindicato;
+    char* ip_app;
+    char* puerto_app;
+    char* ruta_log;
+    int posicion_x;
+    int posicion_y;
+} t_cliente_config;
+
+t_cliente_config* cliente_config;
+t_log* logger;
+
+// FUNCIONES
+void cliente_init(t_cliente_config** cliente_config, t_log** logger);
+void cliente_finally(t_cliente_config* cliente_config, t_log* logger);
+t_cliente_config* cliente_config_loader(char* path_config_file);
+void cliente_config_parser(t_config* config, t_cliente_config* cliente_config);
+void cliente_destroy(t_cliente_config* cliente_config);
+
 #endif

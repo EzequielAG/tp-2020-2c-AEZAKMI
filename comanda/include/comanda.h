89130f45118a9484e7a12c12389cc0f39230f972
EzequielAG <gaunaezequiel.a@gmail.com>
2020-09-10 23:19:03 -0300

    [ADD] Configuracion de comanda

--- comanda/include/comanda.h
+++ comanda/include/comanda.h
@@ -2,8 +2,31 @@
 #define COMANDA_H
 #include <stdio.h>
 #include <commons/log.h>
+#include <commons/config.h>
 #include <stdbool.h>
 #include "shared_utils.h"
 #include "server.h"
 #include "tests.h"
+
+// VARIABLES Y ESTRUCTURAS
+typedef struct {
+    char* puerto_escucha;
+    char* esquema_memoria;
+    int tamanio_memoria;
+    int tamanio_swap;
+    int frecuencia_compactacion;
+    char* algoritmo_reemplazo;
+    char* ruta_log;
+} t_comanda_config;
+
+t_comanda_config* comanda_config;
+t_log* logger;
+
+// FUNCIONES
+void comanda_init(t_comanda_config** comanda_config, t_log** logger);
+void comanda_finally(t_comanda_config* comanda_config, t_log* logger);
+t_comanda_config* comanda_config_loader(char* path_config_file);
+void comanda_config_parser(t_config* config, t_comanda_config* comanda_config);
+void comanda_destroy(t_comanda_config* comanda_config);
+
 #endif
--- comanda/src/comanda.c
+++ comanda/src/comanda.c
@@ -1,9 +1,47 @@
 #include "comanda.h"
-int main(void){
-//ESTE VA A ACTUAR DE SERVER
-     t_log* logger = log_create("./cfg/comanda.log", "COMANDA", true, LOG_LEVEL_INFO);
-    log_info(logger, "Soy el MODULO COMANDA! %s", mi_funcion_compartida());
+int main(int argc, char *argv[]){
+    comanda_init(&comanda_config, &logger);
 
-     inicio_server("127.0.0.1", "6010",logger);
-        log_destroy(logger);
+    printf("Imprimiendo el path %s", comanda_config->ruta_log);
+
+    comanda_finally(comanda_config, logger);
+    return 0;
+}
+
+void comanda_init(t_comanda_config** comanda_config, t_log** logger){
+    *comanda_config = comanda_config_loader("./cfg/comanda.config");
+    *logger = init_logger((*comanda_config)->ruta_log, "comanda", LOG_LEVEL_INFO);
+}
+
+void comanda_finally(t_comanda_config* comanda_config, t_log* logger) {
+    comanda_destroy(comanda_config);
+    log_destroy(logger);
+}
+
+t_comanda_config* comanda_config_loader(char* path_config_file) {
+    t_config* config = config_create(path_config_file);
+    t_comanda_config* comanda_config = malloc(sizeof(t_comanda_config));
+
+    comanda_config_parser(config, comanda_config);
+    config_destroy(config);
+
+    return comanda_config;
+}
+
+void comanda_config_parser(t_config* config, t_comanda_config* comanda_config) {
+    comanda_config->puerto_escucha = strdup(config_get_string_value(config, "PUERTO_ESCUCHA"));
+    comanda_config->esquema_memoria = strdup(config_get_string_value(config, "ESQUEMA_MEMORIA"));
+    comanda_config->tamanio_memoria = config_get_int_value(config, "TAMANIO_MEMORIA");
+    comanda_config->tamanio_swap = config_get_int_value(config, "TAMANIO_SWAP");
+    comanda_config->frecuencia_compactacion = config_get_int_value(config, "FRECUENCIA_COMPACTACION");
+    comanda_config->algoritmo_reemplazo = strdup(config_get_string_value(config, "ARCHIVO_LOG"));
+    comanda_config->ruta_log = strdup(config_get_string_value(config, "ARCHIVO_LOG"));
+}
+
+void comanda_destroy(t_comanda_config* comanda_config) {
+    free(comanda_config->puerto_escucha);
+    free(comanda_config->esquema_memoria);
+    free(comanda_config->algoritmo_reemplazo);
+    free(comanda_config->ruta_log);
+    free(comanda_config);
 }

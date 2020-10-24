f6dd56d7b4e6861c6caa54efedb5a36d52e672cc
Walter <sebastian.taka@hotmail.com>
2020-10-24 13:24:15 -0300

    Base sindicato

--- sindicato/include/sindicato_fs.h
+++ sindicato/include/sindicato_fs.h
@@ -6,9 +6,20 @@
 #include "sindicato_config.h"
 
 // VARIABLES Y ESTRUCTURAS
+typedef struct {
+    char* block_size;
+    char* blocks;
+    char* magic_number;
+} t_sindicato_metadata;
 
+t_sindicato_metadata* sindicato_metadata;
 
 // FUNCIONES
 void get_or_create_fs();
+int get_or_create_metadata();
+int get_or_create_montaje_point();
+void crear_metadata_default();
+void leer_metadata_afip();
+void crear_bitmap();
 
 #endif

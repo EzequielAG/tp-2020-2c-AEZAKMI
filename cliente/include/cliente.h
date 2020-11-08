4db61e5a847b699d16d984548581797ca2b38354
Walter <sebastian.taka@hotmail.com>
2020-11-08 16:17:19 -0300

    arreglo la configuracion cliente

--- cliente/include/cliente.h
+++ cliente/include/cliente.h
@@ -8,14 +8,8 @@
 
 // VARIABLES Y ESTRUCTURAS
 typedef struct {
-    char* ip_comanda;
-    char* puerto_comanda;
-    char* ip_restaurante;
-    char* puerto_restaurante;
-    char* ip_sindicato;
-    char* puerto_sindicato;
-    char* ip_app;
-    char* puerto_app;
+    char* ip;
+    char* puerto;
     char* ruta_log;
     int posicion_x;
     int posicion_y;
@@ -31,8 +25,8 @@ void cliente_finally(t_cliente_config* cliente_config, t_log* logger);
 t_cliente_config* cliente_config_loader(char* path_config_file);
 void cliente_config_parser(t_config* config, t_cliente_config* cliente_config);
 void cliente_destroy(t_cliente_config* cliente_config);
-t_modulo * crear_modulo(char* ip, char* puerto, char* nombre);
-t_modulo* get_modulo_by_name(char* nombreDelModulo);
+t_modulo * crear_modulo(char* ip, char* puerto);
+t_modulo* get_modulo_config();
 int handshake(t_modulo* modulo);
 void escuchar_mensajes_socket_desacoplado(int socket);
 void escuchar_mensajes_socket(t_parameter* parametro);
